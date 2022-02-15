7ba40db8e5ededd1931325656e2271ca9a02baa9
Malvin Tok <kevenitetok@gmail.com>
2022-02-15 11:50:16 +0800

    Rename file, add methods for linked list operation

---
+++ LinkedListMain.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+public class LinkedListMain {
+    public LinkedListMain() {}
+
+    public void addAndSort(LinkedList<Integer> list, int value){}
+
+    public void swapValues(LinkedList<Integer> list, int indexOne, int indexTwo){}
+
+    public void findValue(LinkedList<Integer> list, int searchVal){}
+}
