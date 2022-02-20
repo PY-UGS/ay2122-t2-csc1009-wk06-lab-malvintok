d507bf0f65737353efad04a1b606fe25f8811883
Malvin Tok <kevenitetok@gmail.com>
2022-02-20 16:03:36 +0800

    Create and print list

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -3,6 +3,20 @@ import java.util.LinkedList;
 public class LinkedListMain {
     public LinkedListMain() {}
 
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        list.add(1);
+        list.add(3);
+        list.add(5);
+        list.add(7);
+        list.add(9);
+        list.add(11);
+        System.out.print("List before adding: ");
+        for (int index = 0; index < list.size(); index++){
+            System.out.print(list.get(index) + " ");
+        }
+    }
+
     public void addAndSort(LinkedList<Integer> list, int value){}
 
     public void swapValues(LinkedList<Integer> list, int indexOne, int indexTwo){}
